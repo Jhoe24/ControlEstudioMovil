b7ec16462e6b40bd96f7b5d6042990eee6ffb850
Andy Palma <109931874+Jhoe24@users.noreply.github.com>
2025-09-22 01:56:39 -0400

    update

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ AUTH_USER_MODEL = 'authentication.Usuario'
 
 
 MIDDLEWARE = [
+    'white_noise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'config.urls'
 
